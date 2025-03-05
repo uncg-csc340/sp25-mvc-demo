03e5c2c8f477329a66891a4649d9df9d12358f7f
csc340sp24 <csc340sp24@gmail.com>
2025-03-05 17:52:06 -0500

    added POST forms

--- src/main/java/com/csc340/mvc_demo/student/StudentController.java
+++ src/main/java/com/csc340/mvc_demo/student/StudentController.java
@@ -8,11 +8,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 /**
  * StudentController.java.
- * Includes all REST API endpoint mappings for the Student object.
+ * Includes all MVC mappings for the Student object.
  */
 //@RestController
 @Controller
@@ -46,13 +44,12 @@ public class StudentController {
      */
     @GetMapping("/{studentId}")
     public Object getOneStudent(@PathVariable int studentId, Model model) {
-       // return new ResponseEntity<>(service.getStudentById(studentId), HttpStatus.OK);
-         model.addAttribute("student", service.getStudentById(studentId));
-         model.addAttribute("title", "Student #: "+studentId);
-         return "student-details";
+        // return new ResponseEntity<>(service.getStudentById(studentId), HttpStatus.OK);
+        model.addAttribute("student", service.getStudentById(studentId));
+        model.addAttribute("title", "Student #: " + studentId);
+        return "student-details";
 
     }
-
 
     /**
      * Get a list of students with a name that contains the given string.
@@ -85,7 +82,6 @@ public class StudentController {
         return "student-list";
     }
 
-
     /**
      * Get a list of students with a GPA above a threshold.
      * http://localhost:8080/students/honors?gpa=3.6
@@ -100,31 +96,65 @@ public class StudentController {
     }
 
     /**
+     * Show the view for a new Student Form.
+     *
+     * @param model
+     * @return the form view
+     */
+    @GetMapping("/createForm")
+    public String showCreateForm(Model model) {
+        Student student = new Student();
+        model.addAttribute("student", student);
+        model.addAttribute("title", "Create New Student");
+        return "student-create";
+    }
+
+    /**
      * Create a new Student entry.
-     * http://localhost:8080/students/new --data '{  "name": "sample new student", "major": "csc", "gpa": 3.55}'
+     * http://localhost:8080/students/new
+     *
+     * We do not need the @RequestBody!
      *
      * @param student the new Student object.
      * @return the updated list of Students.
      */
     @PostMapping("/new")
-    public Object addNewStudent(@RequestBody Student student) {
+    public Object addNewStudent(Student student) {
         service.addNewStudent(student);
-        return new ResponseEntity<>(service.getAllStudents(), HttpStatus.CREATED);
+        // return new ResponseEntity<>(service.getAllStudents(), HttpStatus.CREATED);
+        return "redirect:/students/all";
 
     }
 
     /**
+     * Show the update form.
+     *
+     * @param studentId
+     * @param model
+     * @return the update form view.
+     */
+    @GetMapping("/update/{studentId}")
+    public String showUpdateForm(@PathVariable int studentId, Model model) {
+        model.addAttribute("student", service.getStudentById(studentId));
+        model.addAttribute("title", "Update Student");
+        return "student-update";
+    }
+
+    /**
      * Update an existing Student object.
-     * http://localhost:8080/students/update/2 --data '{ "studentId": 1, "name": "sampleUpdated", "major": "csc", "gpa": 3.92}'
+     * http://localhost:8080/students/update/2
+     *
+     * We do not need the @RequestBody!
      *
      * @param studentId the unique Student Id.
      * @param student   the new update Student details.
      * @return the updated Student object.
      */
-    @PutMapping("/update/{studentId}")
-    public Object updateStudent(@PathVariable int studentId, @RequestBody Student student) {
+    @PostMapping("/update/{studentId}")
+    public Object updateStudent(@PathVariable int studentId, Student student) {
         service.updateStudent(studentId, student);
-        return new ResponseEntity<>(service.getStudentById(studentId), HttpStatus.CREATED);
+        //return new ResponseEntity<>(service.getStudentById(studentId), HttpStatus.CREATED);
+        return "redirect:/students/" + studentId;
 
     }
 
@@ -135,9 +165,10 @@ public class StudentController {
      * @param studentId the unique Student Id.
      * @return the updated list of Students.
      */
-    @DeleteMapping("/delete/{studentId}")
+    @GetMapping("/delete/{studentId}")
     public Object deleteStudentById(@PathVariable int studentId) {
         service.deleteStudentById(studentId);
-        return new ResponseEntity<>(service.getAllStudents(), HttpStatus.OK);
+        // return new ResponseEntity<>(service.getAllStudents(), HttpStatus.OK);
+        return "redirect:/students/all";
     }
 }
